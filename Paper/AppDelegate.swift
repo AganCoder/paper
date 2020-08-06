d53fa3a6b45c441bf3e167528b43ac3606a7359b
Noah <noah.gao@zoom.us>
2020-08-06 22:29:42 +0800

    初始化项目

---
+++ Paper/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Paper
+//
+//  Created by Noah on 2020/8/6.
+//  Copyright © 2020 Noah Gao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+}
+
