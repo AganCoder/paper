4e917116253708213e5bdf746bbca963116156b0
Noah <noah.gao@zoom.us>
2020-08-09 23:16:16 +0800

    Show in StatusBar & hidden dock & add click action

--- Paper/AppDelegate.swift
+++ Paper/AppDelegate.swift
@@ -11,10 +11,34 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+    let popover = NSPopover()
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+
+        let icon = NSImage(named: NSImage.Name("icon16*16"))
+        icon?.isTemplate = true
+
+        statusItem.image = icon
+        statusItem.button?.target = self
+        statusItem.button?.action = #selector(onStatusItemDidClicked(_:))
+
+        popover.behavior = .transient
+        popover.animates = true
+        popover.contentSize = CGSize(width: 285, height: 600)
+        popover.contentViewController =  MainPopoverViewController()
+
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
+    @objc private func onStatusItemDidClicked(_ statusBarButton: NSStatusBarButton) {
+
+        debugPrint(#function)
+
+        if !self.popover.isShown {
+            self.popover.show(relativeTo: statusBarButton.bounds, of: statusBarButton, preferredEdge: .minY)
+        }
     }
 }
 
---
+++ Paper/MainPopoverViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainPopoverViewController.swift
+//  Paper
+//
+//  Created by Noah on 2020/8/9.
+//  Copyright © 2020 Noah Gao. All rights reserved.
+//
+
+import Cocoa
+
+class MainPopoverViewController: NSViewController {
+
+    override func loadView() {
+      self.view = NSView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
