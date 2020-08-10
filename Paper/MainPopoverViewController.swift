74c74bd7eef0f66594e3611962d5bcf2c7f5be65
Noah <noah.gao@zoom.us>
2020-08-10 22:48:49 +0800

    add views

--- Paper/MainPopoverViewController.swift
+++ Paper/MainPopoverViewController.swift
@@ -11,13 +11,79 @@ import Cocoa
 class MainPopoverViewController: NSViewController {
 
     override func loadView() {
-      self.view = NSView()
+        self.view = NSView()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        initSubviews()
+    }
 
+    @objc func settingButtonDidTapped(sender: NSButton) {
+        debugPrint("setting")
+    }
+
+    @objc func saveButtonDidTapped(sender: NSButton) {
+        debugPrint("save")
+    }
+
+
+    private func initSubviews() {
+
+        let brand = NSImageView(image: NSImage(named: "newheader280x94")!)
+        brand.imageScaling = .scaleAxesIndependently
+
+        let save = NSButton(image: NSImage(named: "saved-icon16x16")!, target: self, action: #selector(saveButtonDidTapped(sender:)))
+        save.setButtonType(NSButton.ButtonType.pushOnPushOff)
+        save.bezelStyle = .rounded
+        save.isBordered = false
+
+        let setting = NSButton(image: NSImage(named: "Settings16x16")!, target: self, action: #selector(settingButtonDidTapped(sender:)))
+        setting.setButtonType(NSButton.ButtonType.pushOnPushOff)
+        setting.bezelStyle = .rounded
+        setting.isBordered = false
+
+        let tableView = NSTableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .red
+
+        view.addSubview(tableView)
+        view.addSubview(brand)
+        view.addSubview(setting)
+        view.addSubview(save)
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.widthAnchor.constraint(equalToConstant: 285).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 600).isActive = true
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        tableView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+
+        brand.translatesAutoresizingMaskIntoConstraints = false
+        brand.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        brand.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        brand.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        brand.heightAnchor.constraint(equalToConstant: 94).isActive = true
+
+        save.translatesAutoresizingMaskIntoConstraints = false
+        save.centerYAnchor.constraint(equalTo: brand.centerYAnchor).isActive = true
+        save.rightAnchor.constraint(equalTo: setting.leftAnchor, constant: 0).isActive = true
+        save.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        save.heightAnchor.constraint(equalToConstant: 32).isActive = true
+
+        setting.translatesAutoresizingMaskIntoConstraints = false
+        setting.centerYAnchor.constraint(equalTo: save.centerYAnchor).isActive = true
+        setting.rightAnchor.constraint(equalTo: brand.rightAnchor, constant: -16).isActive = true
+        setting.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        setting.heightAnchor.constraint(equalToConstant: 32).isActive = true
+
+        // tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        // tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
     override func viewWillAppear() {
@@ -26,12 +92,23 @@ class MainPopoverViewController: NSViewController {
         // set triangle Part color
         if let superView = self.view.window?.contentView?.superview {
             superView.wantsLayer = true
-            superView.layer?.backgroundColor = NSColor.purple.cgColor
+            superView.layer?.backgroundColor = NSColor.main.cgColor
         }
 
         // set background color
         self.view.wantsLayer = true
-        self.view.layer?.backgroundColor = NSColor.green.cgColor
+        self.view.layer?.backgroundColor = NSColor.background.cgColor
     }
 
+}
+
+extension MainPopoverViewController: NSTableViewDelegate {
+
+}
+
+extension MainPopoverViewController: NSTableViewDataSource {
+
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 0
+    }
 }
---
+++ Paper/NSColor+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  NSColor+Extension.swift
+//  Paper
+//
+//  Created by Noah on 2020/8/10.
+//  Copyright © 2020 Noah Gao. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+extension NSColor {
+    static var main = NSColor(red: 34.0/255.0, green: 34.0/255.0, blue: 34.0/255.0, alpha: 1.0)
+
+    static var background = NSColor(red: 44.0/255.0, green: 44.0/255.0, blue: 44.0/255.0, alpha: 1.0)
+}
